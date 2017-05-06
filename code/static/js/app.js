e6b95851c1d155881743dc42d648306e33b8f4ab
Paul Michael Núñez P <mike@nunez.pro>
2017-05-06 14:59:52 -0500

    Fix static, ptoject now support Bootstrap

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -1,1 +1,32 @@
-$(document).foundation()
+$("form").keypress(
+    function(event){
+     if (event.which == '13') {
+        event.preventDefault();
+      }
+});
+
+
+$("#id_ruc").keypress(
+    function(event){
+     if (event.which == '13') {
+        popu_client($(this).val());
+      }
+});
+
+
+function popu_client(v) {
+	$.ajax({
+		url: "/facturacion",
+		method: "POST",
+		data: { 
+			csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
+			cli_val: v },
+		dataType: "json",
+		})
+		.done(function( data ) {
+			$("#id_firstname").val(data.nombre);
+			$("#id_lastname").val(data.apellido);
+			$("#id_phone").val(data.phone);
+			$("#id_email").val(data.email);
+		});
+}
