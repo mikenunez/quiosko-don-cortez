fe57a0f973c0ede2cc2406bc8eeae362b51c8bd9
Paul Michael Núñez P <mike@nunez.pro>
2017-05-10 15:39:05 -0500

    Fix Reporte Facturas

--- code/reporte/views.py
+++ code/reporte/views.py
@@ -1,15 +1,11 @@
+from django.db.models import FloatField, Sum, F
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.list import ListView
 from facturacion.models import DocumentoCabecera, DocumentoDetalle
 
 # Create your views here.
-class ReporteFacturaPage(View):
-
-	def get(self, request, *args, **kwargs):
-		facturas = DocumentoCabecera.objects.all()
-		context = {
-			'title': '',
-			'description': '',
-			'facturas': facturas
-		}
-		return render(request,"facturas.html", context)
+class ReporteFacturaPage(ListView):
+	model = DocumentoCabecera
+	template_name = "facturas.html"
+	queryset = DocumentoCabecera.objects.annotate(Sum(F('documentodetalle__subtotal')))
