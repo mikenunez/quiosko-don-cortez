a0e7205b128f089743eba2a39138c46043a6431a
Paul Michael Núñez P <mike@nunez.pro>
2017-05-10 10:28:39 -0500

    Update reporte views, rename template

--- code/reporte/views.py
+++ code/reporte/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from facturacion.models import DocumentoCabecera, DocumentoDetalle
+
 # Create your views here.
 class ReporteFacturaPage(View):
 
 	def get(self, request, *args, **kwargs):
+		facturas = DocumentoCabecera.objects.all()
 		context = {
 			'title': '',
-			'description': ''
+			'description': '',
+			'facturas': facturas
 		}
-		return render(request,"reporte-facturas.html", context)
+		return render(request,"facturas.html", context)
