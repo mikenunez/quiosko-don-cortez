07559c4dbf95269fa662f37aaa0ace539fe57c5b
Paul Michael Núñez P <mike@nunez.pro>
2017-05-12 08:53:16 -0500

    Add base template for report2PDF, add urls and view

--- code/reporte/urls.py
+++ code/reporte/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'diario$', views.ReporteDiarioPage.as_view(), name='reporte_diario'),
+    url(r'pdf$', views.ReporteDiario2PDF.as_view(), name='reportediario2pdf'),
 ]
--- code/reporte/views.py
+++ code/reporte/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import FloatField, Sum, F
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.timezone import datetime
 from django.views import View
 
+from quiosko.utils import render_to_pdf
 from facturacion.models import DocumentoCabecera, DocumentoDetalle
 from .forms import FormReporteDiario
 
@@ -36,3 +38,30 @@ class ReporteDiarioPage(LoginRequiredMixin, View):
 		}
 		return render(request,"reporte_diario.html",context)
 		
+
+class ReporteDiario2PDF(View):
+	fecha 		= datetime.today()
+
+	def get(self, request, *args, **kwargs):
+		if request.GET.get('fecha'):
+			self.fecha = request.GET.get('fecha')
+		qs = DocumentoCabecera.objects.filter(created__date=self.fecha).annotate(Sum(F('documentodetalle__subtotal')))
+		context = {
+			'title': '',
+			'description': '',
+			'fecha': self.fecha,
+			'user': request.user,
+			'facturas': qs
+		}
+		pdf = render_to_pdf('reportediario2pdf.html', context)
+		# return render(request,"reportediario2pdf.html",context)
+		if pdf:
+			response = HttpResponse(pdf, content_type='application/pdf')
+			filename = "Reporte_%s.pdf" %(self.fecha)
+			content = "inline; filename='%s'" %(filename)
+			download = request.GET.get("download")
+			if download:
+				content = "attachment; filename='%s'" %(filename)
+			response['Content-Disposition'] = content
+			return response
+		return HttpResponse("Not found")
