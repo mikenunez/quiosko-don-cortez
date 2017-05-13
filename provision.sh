ec871c1a5be225d8f5ac35c5e2d33edbf3f15d96
Paul Michael Núñez P <mike@nunez.pro>
2017-05-13 16:56:40 -0500

    Update requirements.txt

--- provision.sh
+++ provision.sh
@@ -6,11 +6,12 @@ sudo add-apt-repository ppa:nginx/stable -y
 sudo apt-get update -y
 y '' | sudo apt-get install -y nginx
 ## Install Databases
-sudo apt-get install -y postgresql postgresql-contrib python-psycopg2 libpq-dev python3-dev
 rm /usr/share/quisko-don-cortez.zip
 rm -rf /usr/share/quisko-don-cortez
-curl -L https://github.com/mikenunez/quiosko-don-cortez/archive/master.zip -o /usr/share/quisko-don-cortez.zip
+curl -L https://github.com/mikenunez/quiosko-don-cortez/archive/master.zip -o /usr/share/
 unzip -uo quisko-don-cortez.zip -d /usr/share/quisko-don-cortez/
+pip install -U pip
+pip install virtualenv
 exec gunicorn quiosko.wsgi -b 127.0.0.1:8000 --chdir /usr/share/quisko-don-cortez/code
 sudo cp -f /usr/share/quiosko-don-cortez/default /etc/nginx/sites-enabled/default
 sudo service nginx restart
