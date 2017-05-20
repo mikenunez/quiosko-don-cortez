44037bdc27a0d8ce8f6790f944913c1f51c2c7fb
Paul Michael Núñez <mike@nunez.pro>
2017-05-19 20:50:41 -0500

    Update Facturacion.DocDetalleForm iva text

--- code/facturacion/forms.py
+++ code/facturacion/forms.py
@@ -23,6 +23,6 @@ class FormDocDetalle(forms.Form):
 	cantidad 	= forms.IntegerField(required=True, widget=forms.TextInput(attrs={'size':5}))
 	producto 	= forms.ModelChoiceField(queryset=Producto.objects.all())
 	precio_uni 	= forms.DecimalField(decimal_places=2, max_digits=6, label='Precio Unit.', widget=forms.TextInput(attrs={'size':5}), disabled=False)
-	iva			= forms.ModelChoiceField(queryset=TablaCatalogo.objects.all(), to_field_name="code", label='IVA %', required=True, initial='010100')
+	iva			= forms.IntegerField(required=True, widget=forms.TextInput(attrs={'readonly':'readonly', 'size':5}))
 	descuento 	= forms.DecimalField(initial=0.00, decimal_places=2, max_digits=6, widget=forms.TextInput(attrs={'size':5}))
 	subtotal	= forms.DecimalField(decimal_places=2, max_digits=6, disabled=False, widget=forms.TextInput(attrs={'readonly':'readonly', 'size':8}))
