5619f4f0d8fcb56e70ed722bc1b0d1d44b463da8
Paul Michael Núñez <mike@nunez.pro>
2017-05-22 09:53:27 -0500

    make product price field readonly

--- code/facturacion/forms.py
+++ code/facturacion/forms.py
@@ -20,9 +20,9 @@ class FormDocCabecera(forms.ModelForm):
 
 
 class FormDocDetalle(forms.Form):
-	cantidad 	= forms.IntegerField(required=True, widget=forms.TextInput(attrs={'size':5}))
+	cantidad 	= forms.IntegerField(required=True, widget=forms.TextInput(attrs={'size': 5}))
 	producto 	= forms.ModelChoiceField(queryset=Producto.objects.all())
-	precio_uni 	= forms.DecimalField(decimal_places=2, max_digits=6, label='Precio Unit.', widget=forms.TextInput(attrs={'size':5}), disabled=False)
+	precio_uni 	= forms.DecimalField(decimal_places=2, max_digits=6, label='Precio Unit.', widget=forms.TextInput(attrs={'readonly': 'readonly', 'size': 5}), disabled=False)
 	iva			= forms.IntegerField(required=True, widget=forms.TextInput(attrs={'readonly':'readonly', 'size':5}))
 	iva_code	= forms.ModelChoiceField(queryset=TablaCatalogo.objects.all(), widget=forms.HiddenInput())
 	descuento 	= forms.DecimalField(initial=0.00, decimal_places=2, max_digits=6, widget=forms.TextInput(attrs={'size':5}))
